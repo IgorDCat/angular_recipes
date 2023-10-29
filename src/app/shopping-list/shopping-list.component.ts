ff9ce8768c664f7b48471eb024e901404737e233
IgorDCat <powerwolf891028@gmail.com>
2023-10-29 23:46:06 +0300

    added ingredient model, shoping-edit, shoping-list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Тесто', 1),
+    new Ingredient('Сыр', 1),
+    new Ingredient('Помидоры', 2),
+  ]
 }
