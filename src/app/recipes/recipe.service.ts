e01333f250fd8689eb57b727177d9dd7d5690f00
IgorDCat <powerwolf891028@gmail.com>
2023-10-31 19:26:22 +0300

    updated recipe-model, recipe-service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   selectRecipe = new EventEmitter<Recipe>()
@@ -8,12 +9,22 @@ export class RecipeService {
     new Recipe(
       'Пицца',
       'Рецепт вкусной пиццы',
-      'https://cs8.pikabu.ru/avatars/2607/x2607423-868906487.png'
+      'https://avatars.mds.yandex.net/i?id=964e7da484b21017f36de1a9fdb714ed_l-5735248-images-thumbs&n=13',
+      [
+        new Ingredient('Тесто', 1),
+        new Ingredient('Сыр', 1),
+        new Ingredient('Помидоры', 1)
+      ]
     ),
     new Recipe(
-      'Пицца2',
-      'Рецепт вкусной пиццы 2',
-      'https://cs8.pikabu.ru/avatars/2607/x2607423-868906487.png'
+      'Бургер',
+      'Рецепт вкусного бургера',
+      'https://obed-mytishi.ru/wp-content/uploads/2023/06/burg.jpg',
+      [
+        new Ingredient('Булочка', 2),
+        new Ingredient('Мясо', 1),
+        new Ingredient('Помидоры', 1)
+      ]
     ),
   ]
   getRecipes() {
